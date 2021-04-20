98d96bf8576f0f8886642f20d445dd9530ef0d10
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2021-04-20 21:38:07 +0200

    start to translate code

---
+++ DiMarco/DiMarco/IMainCharacter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DiMarco;
+
+namespace DiMarco
+{
+    public interface IMainCharacter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        double MaxLife { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        double Life { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        int Money { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="damage"></param>
+        void IncreaseDamage(int damage);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="speed"></param>
+        void IncreaseSpeed(int speed);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bulletSpeed"></param>
+        void IncreaseBulletSpeed(int bulletSpeed);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="damage"></param>
+        void TakesDamage(int damage);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shoot"></param>
+        /// <param name="vectorDirection"></param>
+        void SetShoot(bool shoot, VectorDirection vectorDirection);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void PickedUpFinalArtifact();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        bool IsDead();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        bool IsWin();
+
+    }
+}
+
---
+++ DiMarco/DiMarco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiMarco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
