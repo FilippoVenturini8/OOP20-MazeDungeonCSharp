391a0fbd9b2fe5d016db04e5cd44a94ea209aaa8
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2021-04-20 17:55:22 +0200

    Start Venturini's project

---
+++ Venturini/Venturini/Point2D.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venturini
+{
+    class Point2D
+    {
+        private double x;
+        private double y; 
+
+        public double X
+        {
+            get { return x; }
+            set { this.x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { this.y = value; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x">the x coordinate of the point.</param>
+        /// <param name="y">the y coordinate of the point.</param>
+        public Point2D(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">the vector to be added.</param>
+        /// <returns>the resultant Point</returns>
+        public Point2D Sum(Vector2D v)
+        {
+            return new Point2D(this.X + v.X, this.y + v.Y);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">the vector to be multiplied.</param>
+        /// <returns>the resultant Point</returns>
+        public Point2D Mul(double v)
+        {
+            return new Point2D(this.x * v, this.y * v);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">the vector to be subtracted.</param>
+        /// <returns>the resultant Point</returns>
+        public Vector2D Sub(Point2D v)
+        {
+            return new Vector2D(this.x - v.X, this.y - v.Y);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>the string representation of a Point2D</returns>
+        public override String ToString()
+        {
+            return "Point2D [x=" + x + ", y=" + y + "]";
+        }
+    }
+}
---
+++ Venturini/Venturini/Vector2D.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venturini
+{
+    class Vector2D
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get { return x; }
+            set { this.x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { this.y = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x">the initial x of the vector.</param>
+        /// <param name="y">the initial y of the vector.</param>
+        public Vector2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="to">starting point of the vector</param>
+        /// <param name="from">ending point of the vector</param>
+        public Vector2D(Point2D to, Point2D from)
+        {
+            this.X = to.X - from.X;
+            this.Y = to.Y - from.Y;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">the vector to be added.</param>
+        /// <returns>the resultant vector</returns>
+        public Vector2D Sum(Vector2D v)
+        {
+            return new Vector2D(this.X + v.X, this.Y + v.Y);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>the module of the vector</returns>
+        public double Module()
+        {
+            return (double)Math.Sqrt(X * X + Y * Y);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fact">the number to be multiplied.</param>
+        /// <returns>the resultant vector</returns>
+        public Vector2D Mul(double fact)
+        {
+            return new Vector2D(X * fact, Y * fact);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>the normalized vector</returns>
+        public Vector2D GetNormalized()
+        {
+            double module = this.Module();
+            return new Vector2D(X / module, Y / module);
+        }
+    }
+}
